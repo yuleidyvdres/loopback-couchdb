d613cd091646e1a2f6d57b2d824f47fdd8863ab9
Yuleidy Valladares <yv@plusteam.tech>
2019-09-09 13:55:04 -0400

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import {LoopbackCouchdbApplication} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {LoopbackCouchdbApplication};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new LoopbackCouchdbApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
